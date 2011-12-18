439a361de778988f2380046cb6b46ddf16fb0354
Frank Pinto <frangucc@gmail.com>
2011-12-17 22:37:12 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Billably::Application.config.secret_token = '30cbd9fd83269d602730ce9464cc62aefbec608c9b4c55e96081682916fbef54976eb43e7408fa06b9f2aaf0043f883f895eca5a9c627eb7a6586998e211d379'
