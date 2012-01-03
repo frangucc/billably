910bf9c571fcf21e62f790b0068b1da1c3a6fe38
Verónica Rebagliatte <rebagliatte@gmail.com>
2012-01-03 13:09:40 -0200

    Pretty page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
   def tab_class(name)
     return '' unless params[:controller]
     controller = params[:controller].split("/").last
