c1e01d1d67826a93f4a55c05f40c5f80188ea171
Verónica Rebagliatte <rebagliatte@gmail.com>
2012-01-04 15:47:26 -0200

    Added new edit and index views for the subscriptions

--- app/controllers/admin/subscriptions_controller.rb
+++ app/controllers/admin/subscriptions_controller.rb
@@ -2,11 +2,11 @@ class Admin::SubscriptionsController < Admin::AdminController
   inherit_resources
 
   private
-
     def showed_columns
       %w(
         area etc featured fine_print long_description
         merchant_id name offering order refundable
         shipping_info shipping_radius_in_miles ships_nationally zipcode )
+      @categories = Category.all
     end
 end
