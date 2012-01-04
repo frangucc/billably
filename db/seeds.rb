869d67fa38063c11953bcdbc12d0dc27564b7f7b
frangucc <frangucc@gmail.com>
2012-01-04 14:37:41 -0600

    added data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,17 @@ Dir[Rails.root.join('db', 'seeds', '*.rb').to_s].each do |file|
   puts "Loading db/seeds/#{file.split(File::SEPARATOR).last}"
   load(file)
 end
+
+
+["Magazines", "Shoes", "Fruits", "Organic", "Wine", "Microbrew", "Farmers Market", "Fresh Fish", "Nutrition", "Education", "Health", "Beauty", "Cosmetics", "Fashion", "Accessories", "Fruits"].each do |cat|
+  Category.find_or_create_by_name(cat)
+end
+
+Merchant.find_or_create_by_name(:name => 'DAmatos Bakery', :city => "Chicago", :state => "IL", :zipcode => "60654", :email => "info@damatos.com")
+Merchant.find_or_create_by_name(:name => 'Soap.com', :city => "Chicago", :state => "IL", :zipcode => "60654", :email => "info@soap.com")
+Merchant.find_or_create_by_name(:name => 'Wag.com', :city => "Chicago", :state => "IL", :zipcode => "60654", :email => "info@wag.com")
+Merchant.find_or_create_by_name(:name => 'Bari Foods', :city => "Chicago", :state => "IL", :zipcode => "60654", :email => "info@barifoods.com")
+Merchant.find_or_create_by_name(:name => 'Harry and David', :city => "Chicago", :state => "IL", :zipcode => "60068", :email => "info@harryanddavid.com")
+Merchant.find_or_create_by_name(:name => 'Jenny Craig', :city => "Chicago", :state => "IL", :zipcode => "60707", :email => "info@jenny.com")
+Merchant.find_or_create_by_name(:name => 'Nutrisystem', :city => "Chicago", :state => "IL", :zipcode => "60610", :email => "info@nutrisystem.com")
+Subscription.find_or_create_by_name(:area => "Chicago", :featured => "1", :merchant_id => "1", :name => "Fresh Bread", :order => "1", :zipcode => "60068", :description => "Get famous DAmatos Fresh Bread delivered to your home, no minimums, just amazing bread.", :fine_print => "Limit 2 per customer", :shipping_radius_in_miles => "20", :shipping_info => "Fedex")
